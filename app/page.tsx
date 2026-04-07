ce53205f11c0f3bfe284caf814aab0ed9acd665d
Abhigyan Chakraborty <abhigyanchakraborty65@gmail.com>
2026-04-08 01:15:06 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>MAKAUT Helper 🚀</h1>
+      <p>Your AI Exam Assistant</p>
+    </main>
+  );
+}
