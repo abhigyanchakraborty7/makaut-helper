5a6505bb660a0afb017909df574c4aefbc69f1f4
Abhigyan Chakraborty <abhigyanchakraborty65@gmail.com>
2026-04-08 14:45:14 +0530

    Add Important Questions Predictor card to features grid

--- app/page.js
+++ app/page.js
@@ -206,6 +206,7 @@ export default function Home() {
               </div>
             ))}
             <NotesGeneratorCard />
+            <ImportantQuestionsCard /> 
           </div>
         </section>
 
