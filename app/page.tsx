5c9a1a2067b7c7c0d27901677d41b863593b85e3
Abhigyan Chakraborty <abhigyanchakraborty65@gmail.com>
2026-04-08 12:17:53 +0530

    Add homepage design

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>MAKAUT Helper 🚀</h1>
-      <p>Your AI Exam Assistant</p>
-    </main>
-  );
-}
